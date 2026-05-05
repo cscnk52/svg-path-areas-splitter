70b140d877ae19e94fd6b763c946b717466868ef
Cason Kervis <cscnk52@outlook.com>
2026-05-05 22:31:42 +0800

    chore: update test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["test/**/*.test.ts"],
     coverage: {
       provider: "istanbul",
     },
     typecheck: {
       enabled: true,
-      include: ["test/type/**/*.test.type.ts"],
     },
   },
   resolve: {
